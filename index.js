2cd4e305f8e2feb99e38cbbe171e9a2ae942b69e
Your Name <you@example.com>
2022-10-03 14:00:52 -0500

    Creating files and importing

---
+++ index.js
@@ -0,0 +1,12 @@
+//All imported files
+const generateHTML = require("./src/generateHTML");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+
+
