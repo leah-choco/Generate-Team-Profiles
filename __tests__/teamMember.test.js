d1171e46058128234c4218d607e1b49bf9b9c62e
Your Name <you@example.com>
2022-10-10 15:56:23 -0500

    Changed teamMember variable to member and now test passing

--- __tests__/teamMember.test.js
+++ __tests__/teamMember.test.js
@@ -1,42 +1,36 @@
-
 const teamMember = require("../lib/teamMember");
 
-
-
-
-
- 
 test('creates a teamMember object', () => {
-    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+    const member = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-    expect(teamMember.name).toEqual(expect.any(String));
-    expect(teamMember.id).toEqual(expect.any(Number));
-    expect(teamMember.email).toEqual(expect.any(String));
+    expect(member.name).toEqual(expect.any(String));
+    expect(member.id).toEqual(expect.any(Number));
+    expect(member.email).toEqual(expect.any(String));
 });
 
  
 test('gets teamMember name', () => {
-    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+    const member = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-    expect(teamMember.getName()).toEqual(expect.any(String));
+    expect(member.getName()).toEqual(expect.any(String));
 });
 
 
 test('gets teamMember ID', () => {
-    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+    const member = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-    expect(teamMember.getId()).toEqual(expect.any(Number));
+    expect(member.getId()).toEqual(expect.any(Number));
 });
 
 test('gets teamMember email', () => {
-    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+    const member = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-    expect(teamMember.getEmail()).toEqual(expect.stringContaining(teamMember.email.toString()));
+    expect(member.getEmail()).toEqual(expect.stringContaining(member.email.toString()));
 });
 
 
 test('gets role of teamMember', () => {
-    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+    const member = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-    expect(teamMember.getRole()).toEqual("teamMember");
+    expect(member.getRole()).toEqual("teamMember");
 });
