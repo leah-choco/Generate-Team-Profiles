a0abdbb39406f9f74e188f0b1a077991cc03206a
Your Name <you@example.com>
2022-10-10 15:30:54 -0500

    Rewrote test, still failing

--- __tests__/teamMember.test.js
+++ __tests__/teamMember.test.js
@@ -1,45 +1,42 @@
+
 const teamMember = require("../lib/teamMember");
 
 
 
-describe("teamMember", () => {
-    it("Creates an object named teamMember", () => {
-        const teamMember = new teamMember("Leah", 20, "mrsleahchoco@gmail.com")
 
-        expect(teamMember.name).toEqual(expect.any(String));
-        expect(teamMember.id).toEqual(expect.any(Number));
-        expect(teamMember.email).toEqual(expect.any(String));
-    });
+
+ 
+test('creates a teamMember object', () => {
+    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+
+    expect(teamMember.name).toEqual(expect.any(String));
+    expect(teamMember.id).toEqual(expect.any(Number));
+    expect(teamMember.email).toEqual(expect.any(String));
 });
 
-describe("getName", () => {
-    it("Gets the teamMember name", () => {
-        const teamMember = new teamMember ("Leah", 20, "mrsleahchoco@gmail.com");
+ 
+test('gets teamMember name', () => {
+    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-        expect (teamMember.getName().toEqual(expect.any(String)));
-    });
+    expect(teamMember.getName()).toEqual(expect.any(String));
 });
 
-describe("getID", () => {
-    it("Gets the teamMember ID", () => {
-        const teamMember = new teamMember ("Leah", 20, "mrsleahchoco@gmail.com");
 
-        expect (teamMember.getID().toEqual(expect.any(Number)));
-    });
+test('gets teamMember ID', () => {
+    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+
+    expect(teamMember.getId()).toEqual(expect.any(Number));
 });
 
-describe("getEmail", () => {
-    it("Gets the teamMember email", () => {
-        const teamMember = new teamMember ("Leah", 20, "mrsleahchoco@gmail.com");
+test('gets teamMember email', () => {
+    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
 
-        expect (teamMember.getEmail().toEqual(expect.any(String)));
-    });
+    expect(teamMember.getEmail()).toEqual(expect.stringContaining(teamMember.email.toString()));
 });
 
-describe("getRole", () => {
-    it("Gets the teamMember role", () => {
-        const teamMember = new teamMember ("Leah", 20, "mrsleahchoco@gmail.com");
 
-        expect (teamMember.getRole().toEqual("teamMember"));
-    });
+test('gets role of teamMember', () => {
+    const teamMember = new teamMember('Leah', 20, 'mrsleahchoco@gmail.com');
+
+    expect(teamMember.getRole()).toEqual("teamMember");
 });
